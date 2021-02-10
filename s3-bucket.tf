de6f2fa102e3524704234331004eb197d6457d99
MMeerim <musabaeva.meerim@gmail.com>
2021-02-10 20:16:53 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/Meerim-training/s3-bucket/aws"
   bucket_prefix = "my-s3-bucket"
   acl    = "private"
-  create_bucket = "true"
+  create_bucket = true
 
   versioning = {
     enabled = true
