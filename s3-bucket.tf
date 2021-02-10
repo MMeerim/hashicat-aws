3f80b72afe8dca1ccc770e1cf4cbe4e2aaf72025
MMeerim <musabaeva.meerim@gmail.com>
2021-02-10 20:08:47 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,5 @@
 module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "1.15.0"
-  # insert the 5 required variables here
-  bucket_prefix = "Meerim-training"
-  acl = "private"
+  source  = "app.terraform.io/Meerim-training/s3-bucket/aws"
+  version = "1.17.0"
+  # insert required variables here
 }
